8db2e2d5531898ac3bfddc93aa9b32beaa74959c
RickLikeJD <hugojosevaz@gmail.com>
2025-05-28 16:15:00 -0300

    Cadastro em PHP implementado e funcionando perfeitamente para cadastro (+Criptografando senhas)

--- gama/conexao.php
+++ gama/conexao.php
@@ -3,6 +3,6 @@
     define('HOST', 'localhost');
     define('USER', 'root');
     define('PASS', 'root');
-    define('BASE', 'gama');
+    define('BASE', 'mydb');
 
     $conn = new mysqli(HOST, USER, PASS, BASE);
