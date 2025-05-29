4d4a4fd34708202b1d4e6d03ede6633e6e0d9dbe
Maybach-on <isaquesilvalopespereira2519@gmail.com>
2025-05-29 12:00:49 -0300

    deu merda aq prototipo 3

--- php/parquebommenino.php
+++ php/parquebommenino.php
@@ -10,21 +10,25 @@
 </head>
 <body>
   <div class="container conteudo">
-    <img src="../img/itapiraco1.jpg" alt="Reserva do Itapiracó"> <!-- ajuste o caminho conforme sua estrutura -->
 
+  <!-- Primeiro bloco: imagem à esquerda, texto à direita -->
+  <div class="texto-imagem">
+    <img src="../img/parque-bom-menino-1.jpg" alt="Parque bom menino">
     <div class="texto">
-      <h2>Parque do bom menino</h2>
-      <p>
-       O Parque do Bom Menino localizado no centro da cidade de São Luís é uma unidade de conservação criada pelo decreto municipal n° 43.825, de 24 de abril de 2013 (embora tenha sido inaugurado em 1969), com área de 9.557 m², com trilhas e muitas árvores.
-      </p>
-      <p>         
-        Na área, há espaço para caminhadas, pontos para alongamentos e aparelhos de musculação, pistas longas e largas facilitam os treinos de ciclistas. O ginásio poliesportivo é destinado para treinos e campeonatos de futsal e serve também para a realização de eventos
-      </p>
-      <p>
-       Reformado em 2012, a estrutura foi ampliada, com o reforço da segurança do parque. Guaritas nas duas entradas da área, ronda a pé e motorizada feitas por guardas municipais, ajudam a manter a segurança de quem frequenta o local. Os portões do parque ficam abertos das 5h às 23h durante toda a semana.
-      </p>
+      <h2>Parque do Bom Menino</h2>
+      <p>O Parque do Bom Menino localizado no centro da cidade de São Luís...</p>
+      <p>Na área, há espaço para caminhadas...</p>
+      <p>Reformado em 2012, a estrutura foi ampliada...</p>
     </div>
-    <div class="container trilhas">
-        <h3>Trilhas do Bom Menino</h3>
-        <p>Uma das trilhas mais conhecidas do parque possui aproximadamente 2,96 km de extensão, com um desnível positivo e negativo de 21 metros, sendo considerada de dificuldade fácil. Este percurso circular pode ser completado em cerca de 28 minutos, tornando-se ideal para caminhadas leves e corridas. O ambiente arborizado e as pistas largas também são propícios para o ciclismo e outras atividades físicas.</p>
+  </div>
+
+  <!-- Segundo bloco: imagem à direita, texto à esquerda -->
+  <div class="texto-imagem-reverso">
+    <img src="../img/trilha-bom-menino.jpg" alt="Trilha do Bom Menino">
+    <div class="texto">
+      <h3>Trilhas do Bom Menino</h3>
+      <p>Uma das trilhas mais conhecidas do parque possui aproximadamente 2,96 km de extensão, com um desnível positivo e negativo de 21 metros, sendo considerada de dificuldade fácil. Este percurso circular pode ser completado em cerca de 28 minutos, tornando-se ideal para caminhadas leves e corridas. O ambiente arborizado e as pistas largas também são propícios para o ciclismo e outras atividades físicas.</p>
     </div>
+  </div>
+
+</div>
