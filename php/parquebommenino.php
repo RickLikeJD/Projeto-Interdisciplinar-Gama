058465b737137f63281ae6a8b93c1efd91002dcc
Maybach-on <isaquesilvalopespereira2519@gmail.com>
2025-05-29 11:06:12 -0300

    prototipo do bom minino

--- php/parquebommenino.php
+++ php/parquebommenino.php
@@ -1,1 +1,34 @@
 <?php include 'header.php'; // Seu cabeçalho HTML ?>
+<?php include 'header.php'; // Seu cabeçalho HTML ?>
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Reserva do Itapiracó</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/bomminino.css"> <!-- CSS separado -->
+</head>
+<body>
+  <div class="container conteudo">
+    <img src="../img/itapiraco1.jpg" alt="Reserva do Itapiracó"> <!-- ajuste o caminho conforme sua estrutura -->
+
+    <div class="texto">
+      <h2>Parque do bom menino</h2>
+      <p>
+       O Parque do Bom Menino localizado no centro da cidade de São Luís é uma unidade de conservação criada pelo decreto municipal n° 43.825, de 24 de abril de 2013 (embora tenha sido inaugurado em 1969), com área de 9.557 m², com trilhas e muitas árvores.
+      </p>
+      <p>         
+        Na área, há espaço para caminhadas, pontos para alongamentos e aparelhos de musculação, pistas longas e largas facilitam os treinos de ciclistas. O ginásio poliesportivo é destinado para treinos e campeonatos de futsal e serve também para a realização de eventos
+      </p>
+      <p>
+       Reformado em 2012, a estrutura foi ampliada, com o reforço da segurança do parque. Guaritas nas duas entradas da área, ronda a pé e motorizada feitas por guardas municipais, ajudam a manter a segurança de quem frequenta o local. Os portões do parque ficam abertos das 5h às 23h durante toda a semana.
+      </p>
+    </div>
+    <div class="container trilhas">
+        <h3>Trilhas</h3>
+        <ul>
+            <li>Trilha do Bom Menino</li>
+            <li>Trilha do Parque</li>
+            <li>Trilha da Natureza</li>
+    </div>
