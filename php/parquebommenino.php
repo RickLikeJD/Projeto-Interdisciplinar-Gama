4edbe16d088cbe9de934f7f9a22027143276c81f
Maybach-on <isaquesilvalopespereira2519@gmail.com>
2025-05-29 12:59:07 -0300

    prototipo bomminino 3

--- php/parquebommenino.php
+++ php/parquebommenino.php
@@ -13,18 +13,24 @@
 
   <!-- Primeiro bloco: imagem à esquerda, texto à direita -->
   <div class="texto-imagem">
-    <img src="../img/parque-bom-menino-1.jpg" alt="Parque bom menino">
+    <img src="../img/ParquedoBomMenino.jpg" alt="Parque bom menino">
     <div class="texto">
       <h2>Parque do Bom Menino</h2>
-      <p>O Parque do Bom Menino localizado no centro da cidade de São Luís...</p>
-      <p>Na área, há espaço para caminhadas...</p>
-      <p>Reformado em 2012, a estrutura foi ampliada...</p>
+      <p>O Parque do Bom Menino, situado no centro de São Luís, Maranhão, é uma unidade de conservação urbana que oferece uma variedade de trilhas e espaços para atividades físicas e lazer. Com uma área de 9.557 m², o parque é um refúgio verde em meio à cidade, proporcionando aos visitantes a oportunidade de se conectar com a natureza e praticar exercícios ao ar livre
+
+      </p>
+      <p> 
+        Reformado em 2012, a estrutura foi ampliada, com o reforço da segurança do parque. Guaritas nas duas entradas da área, ronda a pé e motorizada feitas por guardas municipais, ajudam a manter a segurança de quem frequenta o local. Os portões do parque ficam abertos das 5h às 23h durante toda a semana
+      </p>
+      <p>
+        O objetivo da criação do parque foi a promoção de que as áreas verdes fossem preservadas, aumentando, deste modo, a diversidade de espécies da fauna e flora ludovicense, bem como ampliação de espaços recreativos na capital
+      </p>
     </div>
   </div>
 
   <!-- Segundo bloco: imagem à direita, texto à esquerda -->
   <div class="texto-imagem-reverso">
-    <img src="../img/trilha-bom-menino.jpg" alt="Trilha do Bom Menino">
+    <img src="../img/trilhasbommenino.png" alt="Trilha do Bom Menino">
     <div class="texto">
       <h3>Trilhas do Bom Menino</h3>
       <p>Uma das trilhas mais conhecidas do parque possui aproximadamente 2,96 km de extensão, com um desnível positivo e negativo de 21 metros, sendo considerada de dificuldade fácil. Este percurso circular pode ser completado em cerca de 28 minutos, tornando-se ideal para caminhadas leves e corridas. O ambiente arborizado e as pistas largas também são propícios para o ciclismo e outras atividades físicas.</p>
