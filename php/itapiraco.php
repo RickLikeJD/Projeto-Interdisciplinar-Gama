32330ff8fa604b9d06cb0e70494f5489deb17d0f
Maybach-on <isaquesilvalopespereira2519@gmail.com>
2025-05-28 21:28:12 -0300

    deu merda testa ai

---
+++ php/itapiraco.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Itiporacó</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/style2.css">
+</head>
+<body>
+
+  <div class="container mt-5">
+     <div class="topo-imagem">
+    <img src="https://maps.app.goo.gl/bDRQxF72qrZihhZe8" alt="Imagem de topo">
+   <h1>Reserva do Itapiracó</h1>
+    <p>
+      Antes de se tornar uma unidade de conservação, a área abrigava a Estação de Pesquisa do Ministério da Agricultura, sendo espaço utilizado para o cultivo de frutas cítricas e outras árvores frutíferas, além da criação de porcos e aves. Com a desativação das atividades do Campo Experimental do Itapiracó pelo Ministério, a área foi devolvida ao Departamento de Patrimônio da União (DPU).
+    </p>
+    </div>
+    <p>
+      Com o fim das atividades do Ministério, a Secretaria de Meio Ambiente do Maranhão obteve um Termo de Comodato em Brasília para a administração da área
+    </p>
+    <p>
+        Após a realização de estudos, o Decreto nº 15.618 de 23 de junho de 1997 criou a Área de Proteção Ambiental do Itapiracó. A Reserva do Itapiracó é regulamentada como uma unidade de conservação de uso sustentável, classificação que autoriza o acesso do homem aos recursos naturais e até mesmo o povoamento da região
+    </p>
+    <p>
+        Em 2017, o espaço ganhou mais de 15 praças, campos de futebol, futebol de praia e futevôlei, parquinhos infantis, circuito de skate, trilhas ecológicas, pista de cooper e áreas de estacionamento. O parque contém mais de 10 km de áreas para caminhada e foi considerado o maior centro de lazer do estado, também recebendo eventos e programações especiais.
+    </p>
+    <a href="index.php" class="btn btn-primary back-btn">Voltar</a>
+  </div>
+  <div class="sobre-trilha">
+    <h2>Sobre as trilhas </h2> 
+    <p>
+
+    </p>
+
+  </div>
+
+</body>
+</html>
+
