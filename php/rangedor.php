8d06d1ecfbd81461092b12988b1bdec9d2c0110c
RickLikeJD <hugojosevaz@gmail.com>
2025-05-29 18:59:22 -0300

    Inverti a pos da imagem e do parágrafo

--- php/rangedor.php
+++ php/rangedor.php
@@ -10,11 +10,11 @@
     <div class="container">
         <h2>Parque do Rangedor</h2>
         <div class="flex-container">
+        <div class="imagem-container">
+                <img src="../img/rangedor.webp" alt="Descrição da Imagem">
+            </div>
             <div class="texto-container">
             <p class="intro-text">Criado originalmente como Estação Ecológica em 2005 (Decreto nº 21.797) e posteriormente alterado, o Parque do Rangedor é uma unidade de proteção integral. Sua principal função ecológica é a reposição de aquíferos, fundamental para o abastecimento de lençóis freáticos da cidade. Além disso, atua como um regulador térmico em meio à crescente urbanização e abriga uma diversidade de espécies vegetais nativas, como jatobá, pau-pombo, angelim, babaçu e tucum. Projetos de infraestrutura no parque foram planejados para ocupar áreas já degradadas, com iniciativas de reflorestamento com árvores nativas, visando a conservação ambiental..</p>  
-            </div>
-            <div class="imagem-container">
-                <img src="../img/rangedor.webp" alt="Descrição da Imagem">
             </div>
         </div>
     </div>
