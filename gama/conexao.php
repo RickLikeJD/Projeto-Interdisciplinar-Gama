93cdcbd01d36caa5690f14a66431c134b2399e2b
RickLikeJD <hugojosevaz@gmail.com>
2025-11-26 19:37:25 -0300

    arrumei o nome dos headers, e coloquei gitignore pra conexao, nunca mais vcs precisam ficar arrumando senha e mandando pra prod :)

--- gama/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', 'KmhaH157396@');
-    define('BASE', 'mydb');
-
-    $conn = new mysqli(HOST, USER, PASS, BASE);
